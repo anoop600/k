f824cf0fa3d51fac882ca5712f4244478b499af9
anoop <anoop.jain10@gmail.com>
2024-10-09 21:03:42 +0530

    Add debug

--- script.sh
+++ script.sh
@@ -1,3 +1,16 @@
+
+debug() {
+  # Check if the namespace is provided
+  if [ -z "$1" ]; then
+    echo "Usage: debug <namespace>"
+    return 1
+  fi
+
+  NAMESPACE=$1
+
+  # Run the kubectl command with the provided namespace
+  kubectl run temp --rm -it --image nginx:alpine --restart Never --namespace "$NAMESPACE" -- sh
+}
 alias k=kubectl
 export DO="--dry-run=client -o yaml"
 export G="--now=true"
